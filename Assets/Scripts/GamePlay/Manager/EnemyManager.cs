7778869fd99126e5008e46e3a69928c295ca2086
Thong Truong <thong1411998@gmail.com>
2019-12-19 22:42:06 +0700

    -Add turn-based

---
+++ Assets/Scripts/GamePlay/Manager/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SevenSeas
+{
+    public class EnemyManager : MonoBehaviour
+    {
+
+        public static event System.Action OnAllEnemyActivityCompleted;
+
+       void Awake()
+        {
+            EnemyController.OnBoatActivityCompleted += EnemyController_OnBoatActivityCompleted;
+
+        }
+        void OnDestroy()
+       {
+           EnemyController.OnBoatActivityCompleted -= EnemyController_OnBoatActivityCompleted;
+       }
+
+        int currentChangeTurn = 0;
+       private void EnemyController_OnBoatActivityCompleted(BoatController boatController)
+       {
+          if (boatController.GetType() == typeof(EnemyController))
+          {
+              currentChangeTurn++;
+
+              if (currentChangeTurn == transform.childCount)
+              {
+                  currentChangeTurn = 0;
+                  OnAllEnemyActivityCompleted();
+              }
+          }
+       }
+
+       
+    }
+}
+
---
+++ Assets/Scripts/GamePlay/UI/GameOverUIController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUIController : MonoBehaviour
+{
+    public System.Action OnRestartButtonClick; 
+
+    [SerializeField]
+    private Button restartButton;
+    [SerializeField]
+    private Image gameOverImage;
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+
+
+
+
+
+    void Start()
+    {
+        restartButton.onClick.AddListener(() => OnRestartButtonClick());
+    }
+    public void Show()
+    {
+        Display(true);
+        ShowGameOverImage();
+    }
+    public void Hide()
+    {
+        Display(false);
+    }
+
+    void Display(bool isShowing)
+    {
+        canvasGroup.alpha = isShowing ? 1 : 0;
+        canvasGroup.blocksRaycasts = isShowing;
+        canvasGroup.interactable = isShowing;
+    }
+
+    private Coroutine fadeImageCR;
+    void ShowGameOverImage()
+    {
+        if (fadeImageCR != null)
+            StopCoroutine(fadeImageCR);
+         fadeImageCR = StartCoroutine(CR_FadeInImage(gameOverImage));
+
+
+    }
+
+    IEnumerator CR_FadeInImage(Image img)
+    {
+        Color currentColr = img.color;
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            currentColr.a = Mathf.Lerp(0, 1, 1 / t);
+            img.color = currentColr;
+            yield return null;
+        }
+    }
+
+
+}
