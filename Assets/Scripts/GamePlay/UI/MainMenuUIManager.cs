6f70fc13009e3d1b9b3a0d6ddd85adc4b4abd027
Thong Truong <thong1411998@gmail.com>
2019-12-23 22:54:49 +0700

    - Add script to show option ui

--- Assets/Scripts/GamePlay/UI/MainMenuUIManager.cs
+++ Assets/Scripts/GamePlay/UI/MainMenuUIManager.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class MainMenuUIManager : MonoBehaviour
 {
 
+    [SerializeField]
+    private MenuLeftUIController menuLeftUIController;
+    [SerializeField]
+    private ChooseLevelUIController chooseLevelUIController;
+    [SerializeField]
+    private OptionUIController optionUIController;
+
+    void Awake()
+    {
+        menuLeftUIController.OnOptionButtonClick += menuLeftUIController_OnOptionButtonClick;
+        optionUIController.OnCloseButtonClick += optionUIController_OnCloseButtonClick;
+       
+    }
+
+    private void optionUIController_OnCloseButtonClick()
+    {
+        optionUIController.Hide();
+    }
+
+    void OnDestroy()
+    {
+        menuLeftUIController.OnOptionButtonClick -= menuLeftUIController_OnOptionButtonClick;
+        optionUIController.OnCloseButtonClick -= optionUIController_OnCloseButtonClick;
+    }
+
+    private void menuLeftUIController_OnOptionButtonClick()
+    {
+        optionUIController.Show();
+    }
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
